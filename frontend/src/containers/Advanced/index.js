9fbcd7b4933576af747b4371048877534dec7c6e
Eugene <eugenefromrus@gmail.com>
2020-08-03 23:50:52 +1000

    save commit

---
+++ frontend/src/containers/Advanced/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Advanced.sass';
+
+const Advanced = props => {
+  return (
+    <section className="advanced">
+      <div className="advanced__container">
+          <h1>Расширенный поиск</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Advanced;
---
+++ frontend/src/containers/index.js
@@ -0,0 +1,7 @@
+export { default as Main } from "./Main";
+export { default as Categories } from "./Categories";
+export { default as Advanced } from "./Advanced";
+export { default as Profile } from "./Profile";
+// export { default as Transactions } from "./Transactions";
+// export { default as Favorites } from "./Favorites";
+// export { default as Settings } from "./Settings";
